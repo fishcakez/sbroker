cc921b3cb66f0389be211fd778247190442db375
James Fish <james@fishcakez.com>
2015-01-28 17:15:00 +0000

    Bump to 0.4.0

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule SBroker.Mixfile do
 
       def project do
         [app: :sbroker,
-          version: "0.3.0",
+          version: "0.4.0",
           elixir: "~> 1.0",
           deps: deps(),
           source_url: "https://github.com/fishcakez/sbroker",
